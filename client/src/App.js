656819b852493079a0cb3a73880f949e4f2a7afa
cpwill01 <williamming778@gmail.com>
2021-12-29 16:34:34 +0800

    Add table to TaskList page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
+import { useDispatch } from "react-redux";
 
+import { getTasks } from "./actions/tasksAction";
 import NavBar from "./components/NavBar/NavBar";
+import Home from "./views/Home";
 
-function App() {
-  return <NavBar />;
-}
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getTasks());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <NavBar />
+      <Home />
+    </div>
+  );
+};
 
 export default App;
---
+++ client/src/views/Home.js
@@ -0,0 +1,6 @@
+import React from "react";
+import TaskList from "../components/TaskList/TaskList";
+
+const Home = () => <TaskList />;
+
+export default Home;
